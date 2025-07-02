c207970ebc5af630738e72b359db2be044a03090
Chaitanya <chaitanyaravij@gmail.com>
2025-07-02 08:59:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,5 +6,6 @@ model = LogisticRegression(max_iter=200).fit(iris.data, iris.target)
 st.title("Iris Prediction")
 inputs = [st.slider(l, *v) for l, v in zip(['Sepal Length', 'Sepal Width', 'Petal Length', 'Petal Width'],
 [(4.0, 8.0, 5.1), (2.0, 4.5, 3.5), (1.0, 7.0, 1.4), (0.1, 2.5, 0.2)])]
-if st.button('Predict'):
-    st.success(f"Prediction: {iris.target_names[model.predict([inputs])[0]]}")
+if st.button("predict"):
+    result = model.predict([inputs])[0]
+    st.success(f"prediction: {iris.target_names[result]}")
